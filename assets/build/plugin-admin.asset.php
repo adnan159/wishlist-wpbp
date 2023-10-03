258fed68216b8b6397ba84d21c980a88525ef900
adnan159 <osmanhaider159@gmail.com>
2023-10-03 18:56:36 +0600

    develop merged

---
+++ assets/build/plugin-admin.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => 'bc3619b9e86c71cd2b12');
---
+++ assets/build/plugin-public.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '2e7057327559a0ab14f2');
